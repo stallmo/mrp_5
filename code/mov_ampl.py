8b778b33493f3f0ea736b0861bba246f158f5517
f-gibellini <32666574+f-gibellini@users.noreply.github.com>
2017-11-28 17:08:40 +0100

    Update mov_ampl.py

--- code/mov_ampl.py
+++ code/mov_ampl.py
@@ -10,17 +10,48 @@ def mov_amplitude(data, s_joint, m_joint):
     :param data: pandas df to extract feature from
     :param s_joint: still joint, ideal center of the movement
     :param m_joint: moving joint to get the amplitude of
-    :return: 3x3 list of tuples, with x,y,z for min,max,avg
+    :return: pandas ds with frequency,max,min,mean of amplitudes
     '''
-    ret = []
-
-    data = preprocessing.remove_outliers(data)
+   data = preprocessing.remove_outliers(data)
     data = preprocessing.normalize_data(data)
-    data = preprocessing.filter_noise(data, 10)
-    diff = abs(feature_engineering.calculate_joint_differences(data, only_for_columns=[s_joint+'_x', s_joint+'_y',
-                                                                                       s_joint+'_z', m_joint+'_x',m_joint+'_y',m_joint+'_z']))
+    data = preprocessing.filter_noise(data,10)
+    diff = abs(feature_engineering.calculate_joint_differences(data, only_for_columns=[ 'spineMid_x', 'spineMid_y', 'spineMid_z', 'handR_x', 'handR_y', 'handR_z']))
 
-    ret.append((diff.max().tolist(), diff.min().tolist(), diff.mean().tolist()))
+    #get min max and mean of the differences
+    _max = diff.max()
+    _min = diff.min()
+    _mean = diff.mean()
+
+    #counters for frequency
+    c = 0
+    c_x = 0
+    c_y = 0
+    c_z = 0
+
+    #cycle to detect frequency of "ample" actions given the max-mean threshold
+    for i in diff.iterrows():
+        c+=1
+
+        if i[1]["spineMid_x-handR_x"]+_mean["spineMid_x-handR_x"] >=_max["spineMid_x-handR_x"]:
+            c_x +=1
+        if i[1]["spineMid_y-handR_y"]+_mean["spineMid_y-handR_y"] >=_max["spineMid_y-handR_y"]:
+            c_y +=1
+        if i[1]["spineMid_z-handR_z"]+_mean["spineMid_z-handR_z"] >=_max["spineMid_z-handR_z"]:
+            c_z +=1
+
+    #return creation
+    ret = pd.DataFrame(columns=['X', 'Y', 'Z'])
+    ret.loc[0] = [(float(c_x) / c),(float(c_y) / c),(float(c_z) / c)]
+    ret.loc[1] = [_max["spineMid_x-handR_x"],_max["spineMid_y-handR_y"],_max["spineMid_z-handR_z"]]
+    ret.loc[2] = [_min["spineMid_x-handR_x"], _min["spineMid_y-handR_y"], _min["spineMid_z-handR_z"]]
+    ret.loc[3] = [_mean["spineMid_x-handR_x"], _mean["spineMid_y-handR_y"], _mean["spineMid_z-handR_z"]]
+
+    ''' plot creation if wanted
+    plt.plot(range(len(diff['spineMid_x-handR_x'])), diff['spineMid_x-handR_x'])
+    plt.grid(True)
+    plt.show()
+    '''
+
     return ret
 
 
