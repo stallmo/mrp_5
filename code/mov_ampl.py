96670cdaef698e8aa3b87a0c47c737bb8f829b2c
f-gibellini <32666574+f-gibellini@users.noreply.github.com>
2017-11-29 17:29:53 +0100

    Update mov_ampl.py

--- code/mov_ampl.py
+++ code/mov_ampl.py
@@ -1,8 +1,10 @@
 import loading_routines
 import feature_engineering
 import preprocessing
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
 
 
 
@@ -13,10 +15,20 @@ def mov_amplitude(data, s_joint, m_joint):
     :param m_joint: moving joint to get the amplitude of
     :return: pandas ds with frequency,max,min,mean of amplitudes
     '''
+    s_coords = [s_joint+'_x', s_joint+'_y', s_joint+'_z']
+    m_coords = [m_joint + '_x', m_joint + '_y', m_joint + '_z']
+
+
+
+    delta_x = s_coords[0]+ "-" + m_coords[0]
+    delta_y = s_coords[1] + "-"+m_coords[1]
+    delta_z = s_coords[2] + "-"+m_coords[2]
+
+
     data = preprocessing.remove_outliers(data)
     data = preprocessing.normalize_data(data)
     data = preprocessing.filter_noise(data,10)
-    diff = abs(feature_engineering.calculate_joint_differences(data, only_for_columns=[ 'spineMid_x', 'spineMid_y', 'spineMid_z', 'handR_x', 'handR_y', 'handR_z']))
+    diff = abs(feature_engineering.calculate_joint_differences(data, only_for_columns=[s_coords[0],s_coords[1],s_coords[2],m_coords[0],m_coords[1],m_coords[2]]))
 
     #get min max and mean of the differences
     _max = diff.max()
@@ -33,19 +45,17 @@ def mov_amplitude(data, s_joint, m_joint):
     for i in diff.iterrows():
         c+=1
 
-        if i[1]["spineMid_x-handR_x"]+_mean["spineMid_x-handR_x"] >=_max["spineMid_x-handR_x"]:
+        if i[1][delta_x]+_mean[delta_x] >=_max[delta_x]:
             c_x +=1
-        if i[1]["spineMid_y-handR_y"]+_mean["spineMid_y-handR_y"] >=_max["spineMid_y-handR_y"]:
+        if i[1][delta_y]+_mean[delta_y] >=_max[delta_y]:
             c_y +=1
-        if i[1]["spineMid_z-handR_z"]+_mean["spineMid_z-handR_z"] >=_max["spineMid_z-handR_z"]:
+        if i[1][delta_z]+_mean[delta_z] >=_max[delta_z]:
             c_z +=1
 
     #return creation
-    ret = pd.DataFrame(columns=['X', 'Y', 'Z'])
-    ret.loc[0] = [(float(c_x) / c),(float(c_y) / c),(float(c_z) / c)]
-    ret.loc[1] = [_max["spineMid_x-handR_x"],_max["spineMid_y-handR_y"],_max["spineMid_z-handR_z"]]
-    ret.loc[2] = [_min["spineMid_x-handR_x"], _min["spineMid_y-handR_y"], _min["spineMid_z-handR_z"]]
-    ret.loc[3] = [_mean["spineMid_x-handR_x"], _mean["spineMid_y-handR_y"], _mean["spineMid_z-handR_z"]]
+    ret = pd.DataFrame(columns=['ampl_freq_X', 'ampl_freq_Y', 'ampl_freq_Z','ampl_max_X','ampl_max_Y','ampl_max_Z','ampl_min_X','ampl_min_Y','ampl_min_Z','ampl_mean_X','ampl_mean_Y','ampl_mean_Z'])
+    ret.loc[s_joint+'-'+m_joint] = [(float(c_x) / c),(float(c_y) / c),(float(c_z) / c),_max[delta_x],_max[delta_y],_max[delta_z],_min[delta_x], _min[delta_y], _min[delta_z],_mean[delta_x], _mean[delta_y], _mean[delta_z]]
+
 
     ''' plot creation if wanted
     plt.plot(range(len(diff['spineMid_x-handR_x'])), diff['spineMid_x-handR_x'])
@@ -54,6 +64,3 @@ def mov_amplitude(data, s_joint, m_joint):
     '''
 
     return ret
-
-
-
